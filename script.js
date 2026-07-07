0eda21cf9744db7f904f073913beb56b3ef1fceb
abdxyz03 <ayan641255@gmail.com>
2026-07-07 21:42:29 +0600

    Add click event to buttons with alert message

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll(".btn,.btn2").forEach(button=>{
+button.addEventListener("click",function(e){
+e.preventDefault();
+alert("This feature will be available soon!");
+});
+});
