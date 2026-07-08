e4153b78e523ffe0938c7ab6d39c140b61b47d94
abdxyz03 <ayan641255@gmail.com>
2026-07-08 20:33:58 +0600

    Change Firebase Auth SDK version in register.js
    Updated Firebase Auth SDK version from 12.0.0 to 10.12.2.

--- register.js
+++ register.js
@@ -1,22 +1,2 @@
-// Firebase SDK
 import { auth } from "./firebase.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-
-// Register Form
-const form = document.getElementById("registerForm");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const email = form.querySelector('input[type="email"]').value;
-  const password = form.querySelector('input[type="password"]').value;
-
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      alert("Registration Successful!");
-      window.location.href = "login.html";
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
-});
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
