5349054182833eef13c94a13d77c9ad3db45a8f2
abdxyz03 <ayan641255@gmail.com>
2026-07-08 19:58:47 +0600

    Refactor Firebase authentication setup
    Updated Firebase imports and removed initialization code.

--- login.js
+++ login.js
@@ -1,20 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getAuth,
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAxE5FiSctkj17HxlHh0Yc7kX5YEImZ35E",
-  authDomain: "technogigwork.firebaseapp.com",
-  projectId: "technogigwork",
-  storageBucket: "technogigwork.firebasestorage.app",
-  messagingSenderId: "90694708444",
-  appId: "1:90694708444:web:ce4cf2e27b65978f835819"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 const form = document.querySelector("form");
 
